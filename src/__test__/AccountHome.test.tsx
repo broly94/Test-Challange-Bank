f3fe085b7382d133197df56ed976bad9b81e85da
Leonel Carro <leonel.carro94@gmail.com>
2023-08-02 23:23:29 -0300

    Validar que haya datos en el endpoint para consumir, si no mostrar un mensaje en el home

--- src/__test__/AccountHome.test.tsx
+++ src/__test__/AccountHome.test.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import AccountHome from '../page/Home/AccountHome';
 
 describe('Deben estar los botones o no segun la paginación', () => {
-	test('boton para ver mas opciónes', () => {});
+	test('boton para ver mas opciónes', () => {
+		render(<AccountHome />);
+	});
 });
--- src/page/Home/AccountHome.tsx
+++ src/page/Home/AccountHome.tsx
@@ -5,17 +5,20 @@ import { AccountContext } from '../../context/Context';
 import CardAccount from './components/CardAccount';
 
 export default function AccountHome() {
-	const { accounts, setAccounts, setStartIndex, startIndex, accountPerPage } = useContext(AccountContext);
+	let { accounts, setAccounts, setStartIndex, startIndex, accountPerPage } = useContext(AccountContext);
 
 	const getAccounts = async () => {
 		const { data } = (await getAccount()) as Data;
-
+		if (!data) {
+			accounts = [];
+		} else {
+			const DataNoRepeatAndValidate = data.cuentas.filter(
+				(account, index, self) => index === self.findIndex((a) => a.n == account.n && account.n != ' ')
+			);
+			setAccounts(DataNoRepeatAndValidate);
+			return data;
+		}
 		// Hago un filtro para sacar cuentas con id duplicado y cuentas con id vacio.
-		const DataNoRepeatAndValidate = data.cuentas.filter(
-			(account, index, self) => index === self.findIndex((a) => a.n == account.n && account.n != ' ')
-		);
-		setAccounts(DataNoRepeatAndValidate);
-		return data;
 	};
 
 	const showNext = () => {
@@ -31,28 +34,36 @@ export default function AccountHome() {
 	}, []);
 
 	return (
-		<div className='grow h-auto flex flex-row flex-wrap gap-5 justify-center items-center account-component'>
-			<button
-				onClick={showPrev}
-				disabled={startIndex === 0}
-				className={`mr-2 ${
-					startIndex === 0 ? 'hidden' : 'inline-block'
-				} w-60 h-36 bg-green-600 text-center justify-center rounded-md cursor-pointer text-white `}
-			>
-				{'<< Opciones anteriores'}
-			</button>
+		<>
+			{accounts.length == 0 ? (
+				<div className='grow h-auto flex flex-row flex-wrap gap-5 justify-center items-center account-component'>
+					<h1 className='text-2xl font-medium'>No hay cuentas para mostrar</h1>
+				</div>
+			) : (
+				<div className='grow h-auto flex flex-row flex-wrap gap-5 justify-center items-center account-component'>
+					<button
+						onClick={showPrev}
+						disabled={startIndex === 0}
+						className={`mr-2 ${
+							startIndex === 0 ? 'hidden' : 'inline-block'
+						} w-60 h-36 bg-green-600 text-center justify-center rounded-md cursor-pointer text-white `}
+					>
+						{'<< Opciones anteriores'}
+					</button>
 
-			<CardAccount />
+					<CardAccount />
 
-			<button
-				onClick={showNext}
-				disabled={startIndex + accountPerPage >= accounts.length}
-				className={`${
-					startIndex + accountPerPage >= accounts.length ? 'hidden' : 'inline-block'
-				} w-60 h-36 bg-green-600 text-center justify-center rounded-md cursor-pointer text-white`}
-			>
-				{'Más opciones >>'}
-			</button>
-		</div>
+					<button
+						onClick={showNext}
+						disabled={startIndex + accountPerPage >= accounts.length}
+						className={`${
+							startIndex + accountPerPage >= accounts.length ? 'hidden' : 'inline-block'
+						} w-60 h-36 bg-green-600 text-center justify-center rounded-md cursor-pointer text-white`}
+					>
+						{'Más opciones >>'}
+					</button>
+				</div>
+			)}
+		</>
 	);
 }
